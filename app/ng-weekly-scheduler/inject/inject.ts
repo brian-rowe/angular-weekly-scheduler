8bc719317367c050730ce09c35cfc24f33cf9200
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-18 16:14:59 -0400

    Convert 'inject' to proper TypeScript

--- app/ng-weekly-scheduler/inject/inject.ts
+++ app/ng-weekly-scheduler/inject/inject.ts
@@ -1,19 +1,29 @@
-angular.module('weeklyScheduler')
-  .directive('inject', [function () {
+class InjectDirective implements angular.IDirective {
+  static $name = 'inject';
 
-    return {
-      link: function ($scope, $element, $attrs, controller, $transclude) {
-        if (!$transclude) {
-          throw 'Illegal use of ngTransclude directive in the template! No parent directive that requires a transclusion found.';
-        }
-        var innerScope = $scope.$new();
-        $transclude(innerScope, function (clone) {
-          $element.empty();
-          $element.append(clone);
-          $element.on('$destroy', function () {
-            innerScope.$destroy();
-          });
-        });
-      }
-    };
-  }]);
+  link = ($scope: angular.IScope, $element: angular.IAugmentedJQuery, $attrs: angular.IAttributes, controller, $transclude: angular.ITranscludeFunction) => {
+    if (!$transclude) {
+      throw 'Illegal use of ngTransclude directive in the template! No parent directive that requires a transclusion found.';
+    }
+
+    var innerScope = $scope.$new();
+
+    $transclude(innerScope, function (clone) {
+      $element.empty();
+      $element.append(clone);
+      $element.on('$destroy', function () {
+        innerScope.$destroy();
+      });
+    });
+  }
+
+  static Factory() {
+    let directive = () => new InjectDirective();
+    
+    return directive;
+  }
+}
+
+angular
+  .module('weeklyScheduler')
+  .directive(InjectDirective.$name, InjectDirective.Factory());
