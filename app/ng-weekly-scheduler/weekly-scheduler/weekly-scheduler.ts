292152769b65a9c0361b2e0f64229531b594b2eb
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-19 09:48:33 -0400

    convert weekly-scheduler directive to ts class

--- app/ng-weekly-scheduler/weekly-scheduler/weekly-scheduler.ts
+++ app/ng-weekly-scheduler/weekly-scheduler/weekly-scheduler.ts
@@ -21,137 +21,159 @@ class WeeklySchedulerController implements angular.IController {
   public $modelChangeListeners: Function[]; /* TODO type */
 
   $onInit() {
-   // Try to get the i18n service
-   var name = 'weeklySchedulerLocaleService';
+    // Try to get the i18n service
+    var name = 'weeklySchedulerLocaleService';
 
-   if (this.$injector.has(name)) {
-     this.$log.info('The I18N service has successfully been initialized!');
+    if (this.$injector.has(name)) {
+      this.$log.info('The I18N service has successfully been initialized!');
 
-     var localeService: any = this.$injector.get(name); /* TODO type */
-     this.defaultOptions.labels = localeService.getLang();
-   } else {
-     this.$log.info('No I18N found for this module, check the ng module [weeklySchedulerI18N] if you need i18n.');
-   }
+      var localeService: any = this.$injector.get(name); /* TODO type */
+      this.defaultOptions.labels = localeService.getLang();
+    } else {
+      this.$log.info('No I18N found for this module, check the ng module [weeklySchedulerI18N] if you need i18n.');
+    }
 
-   // Will hang our model change listeners
-   this.$modelChangeListeners = []; 
+    // Will hang our model change listeners
+    this.$modelChangeListeners = [];
+  }
+}
+
+class WeeklySchedulerDirective implements angular.IDirective {
+  static $name = 'weeklyScheduler';
+
+  constructor(
+    private $log: angular.ILogService,
+    private $parse: angular.IParseService,
+    private timeService
+  ) {
+  }
+
+  restrict = 'E';
+  require = 'weeklyScheduler';
+  transclude = true;
+  templateUrl = 'ng-weekly-scheduler/weekly-scheduler/weekly-scheduler.html';
+  controller = WeeklySchedulerController.$name;
+  controllerAs = WeeklySchedulerController.$controllerAs;
+
+  link = (scope, element, attrs, schedulerCtrl: WeeklySchedulerController) => {
+    var optionsFn = this.$parse(attrs.options),
+      options = angular.extend(schedulerCtrl.defaultOptions, optionsFn(scope) || {});
+
+    // Get the schedule container element
+    var el = element[0].querySelector(schedulerCtrl.defaultOptions.selector);
+    var self: WeeklySchedulerDirective = this;
+
+    function onModelChange(items) {
+      // Check items are present
+      if (items) {
+
+        // Check items are in an Array
+        if (!angular.isArray(items)) {
+          throw 'You should use weekly-scheduler directive with an Array of items';
+        }
+
+        // Keep track of our model (use it in template)
+        schedulerCtrl.items = items;
+
+        // First calculate configuration
+        schedulerCtrl.config = self.config(items.reduce((result, item) => {
+          var schedules = item.schedules;
+
+          return result.concat(schedules && schedules.length ?
+            // If in multiSlider mode, ensure a schedule array is present on each item
+            // Else only use first element of schedule array
+            (options.monoSchedule ? item.schedules = [schedules[0]] : schedules) :
+            item.schedules = []
+          );
+        }, []), options);
+
+        // Then resize schedule area knowing the number of weeks in scope
+        el.firstChild.style.width = schedulerCtrl.config.nbWeeks / 53 * 200 + '%';
+
+        // Finally, run the sub directives listeners
+        schedulerCtrl.$modelChangeListeners.forEach(function (listener) {
+          listener(schedulerCtrl.config);
+        });
+      }
+    }
+
+    if (el) {
+      // Install mouse scrolling event listener for H scrolling
+      mouseScroll(el, 20);
+
+      scope.$on(CLICK_ON_A_CELL, function (e, data) {
+        zoomInACell(el, e, data);
+      });
+
+      schedulerCtrl.on = {
+        change: function (itemIndex, scheduleIndex, scheduleValue) {
+          var onChangeFunction = this.$parse(attrs.onChange)(scope);
+          if (angular.isFunction(onChangeFunction)) {
+            return onChangeFunction(itemIndex, scheduleIndex, scheduleValue);
+          }
+        }
+      };
+
+      /**
+       * Watch the model items
+       */
+      scope.$watchCollection(attrs.items, onModelChange);
+
+      /**
+       * Listen to $locale change (brought by external module weeklySchedulerI18N)
+       */
+      scope.$on('weeklySchedulerLocaleChanged', function (e, labels) {
+        if (schedulerCtrl.config) {
+          schedulerCtrl.config.labels = labels;
+        }
+        onModelChange(angular.copy(this.$parse(attrs.items)(scope), []));
+      });
+    }
+  }
+
+  /**
+   * Configure the scheduler.
+   * @param schedules
+   * @param options
+   * @returns {{minDate: *, maxDate: *, nbWeeks: *}}
+   */
+  private config(schedules, options) {
+    var now = moment();
+
+    // Calculate min date of all scheduled events
+    var minDate = (schedules ? schedules.reduce((minDate, slot) => {
+      return this.timeService.compare(slot.start, 'isBefore', minDate);
+    }, now) : now).startOf('week');
+
+    // Calculate max date of all scheduled events
+    var maxDate = (schedules ? schedules.reduce((maxDate, slot) => {
+      return this.timeService.compare(slot.end, 'isAfter', maxDate);
+    }, now) : now).clone().add(1, 'year').endOf('week');
+
+    // Calculate nb of weeks covered by minDate => maxDate
+    var nbWeeks = this.timeService.weekDiff(minDate, maxDate);
+
+    var result = angular.extend(options, { minDate: minDate, maxDate: maxDate, nbWeeks: nbWeeks });
+    // Log configuration
+    this.$log.debug('Weekly Scheduler configuration:', result);
+
+    return result;
+  }
+
+  static Factory() {
+    let directive = ($log, $parse, timeService) => new WeeklySchedulerDirective($log, $parse, timeService);
+
+    directive.$inject = [
+      '$log',
+      '$parse',
+      'weeklySchedulerTimeService'
+    ];
+
+    return directive;
   }
 }
 
 /* global mouseScroll, CLICK_ON_A_CELL, zoomInACell */
 angular.module('weeklyScheduler')
   .controller(WeeklySchedulerController.$name, WeeklySchedulerController)
-  .directive('weeklyScheduler', ['$parse', 'weeklySchedulerTimeService', '$log', function ($parse, timeService, $log) {
-
-    /**
-     * Configure the scheduler.
-     * @param schedules
-     * @param options
-     * @returns {{minDate: *, maxDate: *, nbWeeks: *}}
-     */
-    function config(schedules, options) {
-      var now = moment();
-
-      // Calculate min date of all scheduled events
-      var minDate = (schedules ? schedules.reduce(function (minDate, slot) {
-        return timeService.compare(slot.start, 'isBefore', minDate);
-      }, now) : now).startOf('week');
-
-      // Calculate max date of all scheduled events
-      var maxDate = (schedules ? schedules.reduce(function (maxDate, slot) {
-        return timeService.compare(slot.end, 'isAfter', maxDate);
-      }, now) : now).clone().add(1, 'year').endOf('week');
-
-      // Calculate nb of weeks covered by minDate => maxDate
-      var nbWeeks = timeService.weekDiff(minDate, maxDate);
-
-      var result = angular.extend(options, {minDate: minDate, maxDate: maxDate, nbWeeks: nbWeeks});
-      // Log configuration
-      $log.debug('Weekly Scheduler configuration:', result);
-
-      return result;
-    }
-
-    return {
-      restrict: 'E',
-      require: 'weeklyScheduler',
-      transclude: true,
-      templateUrl: 'ng-weekly-scheduler/weekly-scheduler/weekly-scheduler.html',
-      controller: WeeklySchedulerController.$name,
-      controllerAs: WeeklySchedulerController.$controllerAs,
-      link: function (scope, element, attrs, schedulerCtrl: WeeklySchedulerController) {
-        var optionsFn = $parse(attrs.options),
-          options = angular.extend(schedulerCtrl.defaultOptions, optionsFn(scope) || {});
-
-        // Get the schedule container element
-        var el = element[0].querySelector(schedulerCtrl.defaultOptions.selector);
-
-        function onModelChange(items) {
-          // Check items are present
-          if (items) {
-
-            // Check items are in an Array
-            if (!angular.isArray(items)) {
-              throw 'You should use weekly-scheduler directive with an Array of items';
-            }
-
-            // Keep track of our model (use it in template)
-            schedulerCtrl.items = items;
-
-            // First calculate configuration
-            schedulerCtrl.config = config(items.reduce(function (result, item) {
-              var schedules = item.schedules;
-
-              return result.concat(schedules && schedules.length ?
-                // If in multiSlider mode, ensure a schedule array is present on each item
-                // Else only use first element of schedule array
-                (options.monoSchedule ? item.schedules = [schedules[0]] : schedules) :
-                item.schedules = []
-              );
-            }, []), options);
-
-            // Then resize schedule area knowing the number of weeks in scope
-            el.firstChild.style.width = schedulerCtrl.config.nbWeeks / 53 * 200 + '%';
-
-            // Finally, run the sub directives listeners
-            schedulerCtrl.$modelChangeListeners.forEach(function (listener) {
-              listener(schedulerCtrl.config);
-            });
-          }
-        }
-
-        if (el) {
-          // Install mouse scrolling event listener for H scrolling
-          mouseScroll(el, 20);
-
-          scope.$on(CLICK_ON_A_CELL, function(e, data) {
-            zoomInACell(el, e, data);
-          });
-
-          schedulerCtrl.on = {
-            change: function (itemIndex, scheduleIndex, scheduleValue) {
-              var onChangeFunction = $parse(attrs.onChange)(scope);
-              if (angular.isFunction(onChangeFunction)) {
-                return onChangeFunction(itemIndex, scheduleIndex, scheduleValue);
-              }
-            }
-          };
-
-          /**
-           * Watch the model items
-           */
-          scope.$watchCollection(attrs.items, onModelChange);
-
-          /**
-           * Listen to $locale change (brought by external module weeklySchedulerI18N)
-           */
-          scope.$on('weeklySchedulerLocaleChanged', function (e, labels) {
-            if (schedulerCtrl.config) {
-              schedulerCtrl.config.labels = labels;
-            }
-            onModelChange(angular.copy($parse(attrs.items)(scope), []));
-          });
-        }
-      }
-    };
-  }]);
+  .directive(WeeklySchedulerDirective.$name, WeeklySchedulerDirective.Factory());
