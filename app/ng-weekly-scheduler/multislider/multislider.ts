b2b76e25a94d27bbbbb3186f6cfde8e57f5d5a85
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-19 09:10:03 -0400

    convert MultiSlider directive to TypeScript class

--- app/ng-weekly-scheduler/multislider/multislider.ts
+++ app/ng-weekly-scheduler/multislider/multislider.ts
@@ -1,68 +1,86 @@
-angular.module('weeklyScheduler')
-  .directive('multiSlider', ['weeklySchedulerTimeService', function (timeService) {
-    return {
-      restrict: 'E',
-      require: '^weeklyScheduler',
-      templateUrl: 'ng-weekly-scheduler/multislider/multislider.html',
-      link: function (scope, element, attrs, schedulerCtrl) {
-        var conf = schedulerCtrl.config;
+class MultiSliderDirective implements angular.IDirective {
+  static $name = 'multiSlider';
 
-        // The default scheduler block size when adding a new item
-        var defaultNewScheduleSize = parseInt(attrs.size) || 8;
+  restrict = 'E';
+  require = '^weeklyScheduler';
+  templateUrl = 'ng-weekly-scheduler/multislider/multislider.html';
 
-        var valToPixel = function (val) {
-          var percent = val / (conf.nbWeeks);
-          return Math.floor(percent * element[0].clientWidth + 0.5);
-        };
+  constructor(
+    private timeService
+  ) {
+  }
 
-        var pixelToVal = function (pixel) {
-          var percent = pixel / element[0].clientWidth;
-          return Math.floor(percent * (conf.nbWeeks) + 0.5);
-        };
+  link = (scope, element: angular.IAugmentedJQuery, attrs: angular.IAttributes, schedulerCtrl) => {
+    var conf = schedulerCtrl.config;
 
-        var addSlot = function (start, end) {
-          start = start >= 0 ? start : 0;
-          end = end <= conf.nbWeeks ? end : conf.nbWeeks;
+    // The default scheduler block size when adding a new item
+    var defaultNewScheduleSize = parseInt(attrs.size) || 8;
 
-          var startDate = timeService.addWeek(conf.minDate, start);
-          var endDate = timeService.addWeek(conf.minDate, end);
-
-          scope.$apply(function () {
-            var item = scope.item;
-            if (!item.schedules) {
-              item.schedules = [];
-            }
-            item.schedules.push({start: startDate.toDate(), end: endDate.toDate()});
-          });
-        };
-
-        var hoverElement = angular.element(element.find('div')[0]);
-        var hoverElementWidth = valToPixel(defaultNewScheduleSize);
-
-        hoverElement.css({
-          width: hoverElementWidth + 'px'
-        });
-
-        element.on('mousemove', function (e) {
-          var elOffX = element[0].getBoundingClientRect().left;
-
-          hoverElement.css({
-            left: e.pageX - elOffX - hoverElementWidth / 2 + 'px'
-          });
-        });
-
-        hoverElement.on('click', function (event) {
-          if (!element.attr('no-add')) {
-            var elOffX = element[0].getBoundingClientRect().left;
-            var pixelOnClick = event.pageX - elOffX;
-            var valOnClick = pixelToVal(pixelOnClick);
-
-            var start = Math.round(valOnClick - defaultNewScheduleSize / 2);
-            var end = start + defaultNewScheduleSize;
-
-            addSlot(start, end);
-          }
-        });
-      }
+    var valToPixel = function (val) {
+      var percent = val / (conf.nbWeeks);
+      return Math.floor(percent * element[0].clientWidth + 0.5);
     };
-  }]);
+
+    var pixelToVal = function (pixel) {
+      var percent = pixel / element[0].clientWidth;
+      return Math.floor(percent * (conf.nbWeeks) + 0.5);
+    };
+
+    var addSlot = function (start, end) {
+      start = start >= 0 ? start : 0;
+      end = end <= conf.nbWeeks ? end : conf.nbWeeks;
+
+      var startDate = this.timeService.addWeek(conf.minDate, start);
+      var endDate = this.timeService.addWeek(conf.minDate, end);
+
+      scope.$apply(function () {
+        var item = scope.item;
+        if (!item.schedules) {
+          item.schedules = [];
+        }
+        item.schedules.push({start: startDate.toDate(), end: endDate.toDate()});
+      });
+    };
+
+    var hoverElement = angular.element(element.find('div')[0]);
+    var hoverElementWidth = valToPixel(defaultNewScheduleSize);
+
+    hoverElement.css({
+      width: hoverElementWidth + 'px'
+    });
+
+    element.on('mousemove', function (e) {
+      var elOffX = element[0].getBoundingClientRect().left;
+
+      hoverElement.css({
+        left: e.pageX - elOffX - hoverElementWidth / 2 + 'px'
+      });
+    });
+
+    hoverElement.on('click', function (event) {
+      if (!element.attr('no-add')) {
+        var elOffX = element[0].getBoundingClientRect().left;
+        var pixelOnClick = event.pageX - elOffX;
+        var valOnClick = pixelToVal(pixelOnClick);
+
+        var start = Math.round(valOnClick - defaultNewScheduleSize / 2);
+        var end = start + defaultNewScheduleSize;
+
+        addSlot(start, end);
+      }
+    });
+  }
+
+  static Factory() {
+    let directive = (timeService) => new MultiSliderDirective(timeService);
+
+    directive.$inject = [
+      'weeklySchedulerTimeService'
+    ];
+
+    return directive;
+  }
+}
+
+angular.module('weeklyScheduler')
+  .directive(MultiSliderDirective.$name, MultiSliderDirective.Factory());
