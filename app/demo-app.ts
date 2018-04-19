cbc360324edec4be7e6e49b534aa81dd864f3e6e
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-19 12:33:38 -0400

    Make the days in the demo all close to each other

--- app/demo-app.ts
+++ app/demo-app.ts
@@ -20,7 +20,7 @@ angular.module('demoApp', ['ngAnimate', 'weeklyScheduler', 'weeklySchedulerI18N'
           label: 'Item 1',
           editable: false,
           schedules: [
-            {start: moment('2015-12-27').toDate(), end: moment('2016-08-01').toDate()}
+            {start: moment('2015-12-27').toDate(), end: moment('2016-01-02').toDate()}
           ]
         }]
       };
@@ -29,14 +29,12 @@ angular.module('demoApp', ['ngAnimate', 'weeklyScheduler', 'weeklySchedulerI18N'
         $scope.model.items = $scope.model.items.concat([{
           label: 'Item 2',
           schedules: [
-            {start: moment('2016-05-03').toDate(), end: moment('2017-02-01').toDate()},
-            {start: moment('2015-11-20').toDate(), end: moment('2016-02-01').toDate()}
+            {start: moment('2015-12-25').toDate(), end: moment('2016-01-01').toDate()}
           ]
         }, {
           label: 'Item 3',
           schedules: [
-            {start: moment('2017-08-09').toDate(), end: moment('2017-08-21').toDate()},
-            {start: moment('2017-09-12').toDate(), end: moment('2017-10-12').toDate()}
+            {start: moment('2015-12-26').toDate(), end: moment('2015-12-31').toDate()}
           ]
         }]);
       }, 1000);
