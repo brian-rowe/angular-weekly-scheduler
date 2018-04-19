209cc71ce9ff3e1dfca688672a3508206d5e8dd6
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-19 12:47:54 -0400

    convert WeeklySchedulerTimeService to a TS class

--- app/ng-weekly-scheduler/time/time-services.ts
+++ app/ng-weekly-scheduler/time/time-services.ts
@@ -1,31 +1,34 @@
-angular.module('weeklyScheduler')
-  .service('weeklySchedulerTimeService', [function () {
+class WeeklySchedulerTimeService {
+  static $name = 'weeklySchedulerTimeService';
 
-    var WEEK = 'week';
-    var DAY = 'day';
+  private WEEK = 'week';
 
-    return {
-      compare: function (date, method, lastMin) {
-        if (date) {
-          var dateAsMoment;
-          if (angular.isDate(date)) {
-            dateAsMoment = moment(date);
-          } else if (date._isAMomentObject) {
-            dateAsMoment = date;
-          } else {
-            throw 'Could not parse date [' + date + ']';
-          }
-          return dateAsMoment[method](lastMin) ? dateAsMoment : lastMin;
-        }
-      },
-      addWeek: function (moment, nbWeek) {
-        return moment.clone().add(nbWeek, WEEK);
-      },
-      weekPreciseDiff: function (start, end) {
-        return end.clone().diff(start.clone(), WEEK, true);
-      },
-      weekDiff: function (start, end) {
-        return end.clone().endOf(WEEK).diff(start.clone().startOf(WEEK), WEEK) + 1;
+  public addWeek(moment, nbWeek) {
+    return moment.clone().add(nbWeek, this.WEEK);
+  }
+
+  public compare(date, method, lastMin) {
+    if (date) {
+      var dateAsMoment;
+      if (angular.isDate(date)) {
+        dateAsMoment = moment(date);
+      } else if (date._isAMomentObject) {
+        dateAsMoment = date;
+      } else {
+        throw 'Could not parse date [' + date + ']';
       }
-    };
-  }]);
+      return dateAsMoment[method](lastMin) ? dateAsMoment : lastMin;
+    }
+  }
+
+  public weekPreciseDiff(start, end) {
+    return end.clone().diff(start.clone(), this.WEEK, true);
+  }
+
+  public weekDiff(start, end) {
+    return end.clone().endOf(this.WEEK).diff(start.clone().startOf(this.WEEK), this.WEEK) + 1;
+  }
+}
+
+angular.module('weeklyScheduler')
+  .service(WeeklySchedulerTimeService.$name, WeeklySchedulerTimeService);
