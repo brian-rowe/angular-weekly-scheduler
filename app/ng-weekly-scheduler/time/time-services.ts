14e62be8ea081dbb81021500d2907c69160c4821
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-19 12:42:46 -0400

    remove some code we won't need for the intended final result of this modification

--- app/ng-weekly-scheduler/time/time-services.ts
+++ app/ng-weekly-scheduler/time/time-services.ts
@@ -1,5 +1,5 @@
 angular.module('weeklyScheduler')
-  .service('weeklySchedulerTimeService', ['$filter', function ($filter) {
+  .service('weeklySchedulerTimeService', [function () {
 
     var MONTH = 'month';
     var WEEK = 'week';
@@ -11,7 +11,6 @@ angular.module('weeklyScheduler')
         WEEK: WEEK,
         FORMAT: 'YYYY-MM-DD'
       },
-      dF: $filter('date'),
       compare: function (date, method, lastMin) {
         if (date) {
           var dateAsMoment;
@@ -33,31 +32,6 @@ angular.module('weeklyScheduler')
       },
       weekDiff: function (start, end) {
         return end.clone().endOf(WEEK).diff(start.clone().startOf(WEEK), WEEK) + 1;
-      },
-      monthDiff: function (start, end) {
-        return end.clone().endOf(MONTH).diff(start.clone().startOf(MONTH), MONTH) + 1;
-      },
-      monthDistribution: function (minDate, maxDate) {
-        var i, result = [];
-        var startDate = minDate.clone();
-        var endDate = maxDate.clone();
-        var monthDiff = this.monthDiff(startDate, endDate);
-        var dayDiff = endDate.diff(startDate, DAY);
-
-        //var total = 0, totalDays = 0;
-        // console.log(startDate.toDate(), endDate.toDate(), monthDiff, dayDiff);
-        for (i = 0; i < monthDiff; i++) {
-          var startOfMonth = i === 0 ? startDate : startDate.add(1, MONTH).startOf(MONTH);
-          var endOfMonth = i === monthDiff - 1 ? endDate : startDate.clone().endOf(MONTH);
-          var dayInMonth = endOfMonth.diff(startOfMonth, DAY) + (i !== monthDiff - 1 && 1);
-          var width = Math.floor(dayInMonth / dayDiff * 1E8) / 1E6;
-
-          result.push({start: startOfMonth.clone(), end: endOfMonth.clone(), width: width});
-
-          // totalDays += dayInMonth; total += width;
-          // console.log(startOfMonth, endOfMonth, dayInMonth, dayDiff, width, total, totalDays);
-        }
-        return result;
       }
     };
   }]);
