b09587bb1b904a00b322fa76b2208d44173945bf
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-20 11:54:25 -0400

    continue using nbIntervals * interval instead of nbHours * 60 for the calculations

--- app/ng-weekly-scheduler/multislider/multislider.ts
+++ app/ng-weekly-scheduler/multislider/multislider.ts
@@ -23,7 +23,7 @@ class MultiSliderDirective implements angular.IDirective {
 
     var addSlot = (start, end) => {
       start = start >= 0 ? start : 0;
-      end = end <= conf.nbHours * 60 ? end : conf.nbHours * 60;
+      end = end <= conf.nbIntervals * conf.interval ? end : conf.nbIntervals * conf.interval;
 
       scope.$apply(function () {
         var item = scope.item;
