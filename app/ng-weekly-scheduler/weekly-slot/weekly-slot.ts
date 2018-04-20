df22fc8cac036b25f33522e60c6562178f8cc9d6
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-20 09:00:09 -0400

    fix resizing where resizeDirectionIsStart = false & fix dragging

--- app/ng-weekly-scheduler/weekly-slot/weekly-slot.ts
+++ app/ng-weekly-scheduler/weekly-slot/weekly-slot.ts
@@ -126,7 +126,7 @@ class WeeklySlotDirective implements angular.IDirective {
         } else {
           var newEnd = Math.round(valuesOnDragStart.end + delta);
 
-          if (ui.end !== newEnd && newEnd >= ui.start + 1 && newEnd <= conf.nbHours) {
+          if (ui.end !== newEnd && newEnd >= ui.start + 1 && newEnd <= conf.nbHours * 60) {
             ngModelCtrl.$setViewValue({
               start: ui.start,
               end: newEnd
@@ -138,13 +138,13 @@ class WeeklySlotDirective implements angular.IDirective {
 
       scope.drag = function (d) {
         var ui = ngModelCtrl.$viewValue;
-        var delta = pixelToVal(d);
+        var delta = pixelToVal(d) * 60;
         var duration = valuesOnDragStart.end - valuesOnDragStart.start;
 
         var newStart = Math.round(valuesOnDragStart.start + delta);
         var newEnd = Math.round(newStart + duration);
 
-        if (ui.start !== newStart && newStart >= 0 && newEnd <= conf.nbHours) {
+        if (ui.start !== newStart && newStart >= 0 && newEnd <= conf.nbHours * 60) {
           ngModelCtrl.$setViewValue({
             start: newStart,
             end: newEnd
