08226847bcf0fd591798c1b8b762e55068626ada
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-23 16:10:04 -0400

    Fully convert multiSlider to component

--- app/ng-weekly-scheduler/multislider/multislider.ts
+++ app/ng-weekly-scheduler/multislider/multislider.ts
@@ -1,5 +1,5 @@
 /** @internal */
-class MultiSliderController implements angular.IController {
+class MultiSliderController implements angular.IComponentController {
   static $name = 'multiSliderController';
   static $controllerAs = 'multiSliderCtrl';
 
@@ -13,6 +13,8 @@ class MultiSliderController implements angular.IController {
     this.element = this.$element[0];
   }
 
+  private $hoverElement: angular.IAugmentedJQuery;
+
   public element: Element;
   public config: IWeeklySchedulerConfig;
   public defaultNewScheduleSize: number = 60; // minutes
@@ -23,6 +25,25 @@ class MultiSliderController implements angular.IController {
     if (this.size) {
       this.defaultNewScheduleSize = this.size;
     }
+  }
+
+  $postLink() {
+    this.$hoverElement = angular.element(this.$element.find('div')[0]);
+    let hoverElementWidth = this.valToPixel(this.defaultNewScheduleSize);
+
+    this.$hoverElement.css({
+      width: `${hoverElementWidth}px`
+    });
+
+    this.$element.on('mousemove', (e) => {
+      var elOffX = this.getElementOffsetX(this.$element);
+      var left = e.pageX - elOffX - hoverElementWidth / 2;
+      var snapped = this.valToPixel(this.pixelToVal(left));
+
+      this.$hoverElement.css({
+        left: snapped + 'px'
+      });
+    });
   }
 
   public addSlot(start: number, end: number) {
@@ -58,6 +79,18 @@ class MultiSliderController implements angular.IController {
     return (schedule.end - schedule.start) / this.config.maxValue * 100 + '%';
   }
 
+  private onHoverElementClick(event) {
+    if (!this.$element.attr('no-add')) {
+      var elOffX = this.getElementOffsetX(this.$element);
+      var hoverElOffX = this.getElementOffsetX(this.$hoverElement) - elOffX;
+      
+      var start = this.pixelToVal(hoverElOffX);
+      var end = start + this.defaultNewScheduleSize;
+
+      this.addSlot(start, end);
+    }
+  }
+
   private onWeeklySlotMouseOver() {
     this.$element.addClass('slot-hover');
   }
@@ -78,61 +111,21 @@ class MultiSliderController implements angular.IController {
 }
 
 /** @internal */
-class MultiSliderDirective implements angular.IDirective {
+class MultiSliderComponent implements angular.IComponentOptions {
   static $name = 'multiSlider';
-
-  bindToController = true;
-  controller = MultiSliderController.$name;
-  controllerAs = MultiSliderController.$controllerAs;
-
-  scope = {
+  
+  bindings = {
     config: '<',
     item: '=',
     size: '<?'
   }
 
-  restrict = 'E';
-  require = 'multiSlider';
+  controller = MultiSliderController.$name;
+  controllerAs = MultiSliderController.$controllerAs;
+
   templateUrl = 'ng-weekly-scheduler/multislider/multislider.html';
-
-  link = (scope, element: angular.IAugmentedJQuery, attrs: angular.IAttributes, ctrl: MultiSliderController) => {
-    var hoverElement = angular.element(element.find('div')[0]);
-    var hoverElementWidth = ctrl.valToPixel(ctrl.defaultNewScheduleSize);
-
-    hoverElement.css({
-      width: hoverElementWidth + 'px'
-    });
-
-    element.on('mousemove', function (e) {
-      var elOffX = ctrl.getElementOffsetX(element);
-      var left = e.pageX - elOffX - hoverElementWidth / 2;
-      var snapped = ctrl.valToPixel(ctrl.pixelToVal(left));
-
-      hoverElement.css({
-        left: snapped + 'px'
-      });
-    });
-
-    scope.onHoverElementClick = function(event) {
-      if (!element.attr('no-add')) {
-        var elOffX = ctrl.getElementOffsetX(element);
-        var hoverElOffX = ctrl.getElementOffsetX(hoverElement) - elOffX;
-        
-        var start = ctrl.pixelToVal(hoverElOffX);
-        var end = start + ctrl.defaultNewScheduleSize;
-
-        ctrl.addSlot(start, end);
-      }
-    }
-  }
-
-  static Factory() {
-    let directive = () => new MultiSliderDirective();
-
-    return directive;
-  }
 }
 
 angular.module('weeklyScheduler')
   .controller(MultiSliderController.$name, MultiSliderController)
-  .directive(MultiSliderDirective.$name, MultiSliderDirective.Factory());
+  .component(MultiSliderComponent.$name, new MultiSliderComponent());
