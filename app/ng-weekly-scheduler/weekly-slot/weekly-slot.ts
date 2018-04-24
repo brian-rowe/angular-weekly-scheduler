8e783104e9c164ade8944cd00febef4fa62751d2
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-24 10:18:14 -0400

    Convert weekly-slot to component -- there are bugs here... fixes coming up

--- app/ng-weekly-scheduler/weekly-slot/weekly-slot.ts
+++ app/ng-weekly-scheduler/weekly-slot/weekly-slot.ts
@@ -1,4 +1,4 @@
-class WeeklySlotController implements angular.IController {
+class WeeklySlotController implements angular.IComponentController {
   static $name = 'weeklySlotController';
   static $controllerAs = 'weeklySlotCtrl';
 
@@ -7,216 +7,223 @@ class WeeklySlotController implements angular.IController {
     '$scope'
   ];
 
+  private ngModelCtrl: angular.INgModelController;
+  private schedulerCtrl: WeeklySchedulerController;
+
+  private $containerEl: angular.IAugmentedJQuery;
+  private config: IWeeklySchedulerConfig;
+
   private item: IWeeklySchedulerItem<number>;
   private itemIndex: number;
 
+  private resizeDirectionIsStart: boolean = true;
+
   private schedule: IWeeklySchedulerRange<number>;
   private scheduleIndex: number;
+
+  private valuesOnDragStart: IWeeklySchedulerRange<number>;
 
   constructor(
     private $element: angular.IAugmentedJQuery,
     private $scope: angular.IScope
   ) {
+    this.$containerEl = this.$element.parent();
+  }
+
+  $onInit() {
+    this.valuesOnDragStart = {
+      start: this.schedule.start,
+      end: this.schedule.end
+    };
+
+    this.mergeOverlaps();
+
+    this.ngModelCtrl.$parsers.push((ui) => {
+      this.ngModelCtrl.$modelValue.start = ui.start;
+      this.ngModelCtrl.$modelValue.end = ui.end;
+
+      return this.ngModelCtrl.$modelValue;
+    });
+
+    this.$scope.$on('weeklySchedulerLocaleChanged', () => {
+      // Simple change object reference so that ngModel triggers formatting & rendering
+      this.schedule = angular.copy(this.schedule);
+    });
+  }
+
+  public canRemove() {
+    return !angular.isDefined(this.item.editable) || this.item.editable;
+  }
+
+  public deleteSelf() {
+    this.$containerEl.removeClass('dragging');
+    this.$containerEl.removeClass('slot-hover');
+    this.removeSchedule(this.schedule);
+  }
+
+  public drag(pixel: number) {
+    let ui = this.schedule;
+    let delta = this.pixelToVal(pixel);
+    let duration = this.valuesOnDragStart.end - this.valuesOnDragStart.start;
+
+    let newStart = Math.round(this.valuesOnDragStart.start + delta);
+    let newEnd = Math.round(newStart + duration);
+
+    if (ui.start !== newStart && newStart >= 0 && newEnd <= this.config.maxValue) {
+      this.updateSelf({
+        start: newStart,
+        end: newEnd
+      });
+    }
+  }
+
+  public endDrag() {
+    // this prevents user from accidentally
+    // adding new slot after resizing or dragging
+    setTimeout(() => {
+      this.$containerEl.removeAttr('no-add');
+    }, 500);
+
+    this.$element.removeClass('active');
+    this.$containerEl.removeClass('dragging');
+
+    this.mergeOverlaps();
+  }
+
+  public mergeOverlaps() {
+    let schedule = this.schedule;
+    let schedules = this.item.schedules;
+
+    schedules.forEach((el) => {
+      if (el !== schedule) {
+        // model is inside another slot
+        if (el.end >= schedule.end && el.start <= schedule.start) {
+          this.removeSchedule(el);
+
+          this.updateSelf({
+            start: el.start,
+            end: el.end
+          });
+        }
+        // model completely covers another slot
+        else if (schedule.end >= el.end && schedule.start <= el.start) {
+          this.removeSchedule(el);
+        }
+        // another slot's end is inside current model
+        else if (el.end >= schedule.start && el.end <= schedule.end) {
+          this.removeSchedule(el);
+
+          this.updateSelf({
+            start: el.start,
+            end: schedule.end
+          });
+        }
+        // another slot's start is inside current model
+        else if (el.start >= schedule.start && el.start <= schedule.end) {
+          this.removeSchedule(el);
+
+          this.updateSelf({
+            start: schedule.start,
+            end: el.end
+          });
+        }
+      }
+    });
+  }
+
+  public pixelToVal(pixel: number) {
+    let percent = pixel / this.$containerEl[0].clientWidth;
+    return Math.floor(percent * (this.config.intervalCount) + 0.5) * this.config.interval;
+  }
+
+  public removeSchedule(schedule: IWeeklySchedulerRange<number>) {
+    let schedules = this.item.schedules;
+
+    schedules.splice(schedules.indexOf(schedule), 1);
+  }
+
+  public resize(pixel: number) {
+    let ui = this.schedule;
+    let delta = this.pixelToVal(pixel);
+
+    if (this.resizeDirectionIsStart) {
+      let newStart = Math.round(this.valuesOnDragStart.start + delta);
+
+      if (ui.start !== newStart && newStart <= ui.end - 1 && newStart >= 0) {
+        this.updateSelf({
+          start: newStart,
+          end: ui.end
+        });
+      }
+    } else {
+      let newEnd = Math.round(this.valuesOnDragStart.end + delta);
+
+      if (ui.end !== newEnd && newEnd >= ui.start + 1 && newEnd <= this.config.maxValue) {
+        this.updateSelf({
+          start: ui.start,
+          end: newEnd
+        });
+      }
+    }
+  }
+
+  public startDrag() {
+    this.$element.addClass('active');
+
+    this.$containerEl.addClass('dragging');
+    this.$containerEl.attr('no-add', 'true');
+
+    this.valuesOnDragStart = {
+      start: this.schedule.start,
+      end: this.schedule.end
+    };
+  }
+
+  public startResizeStart() {
+    this.resizeDirectionIsStart = true;
+    this.startDrag();
+  }
+
+  public startResizeEnd() {
+    this.resizeDirectionIsStart = true;
+    this.startDrag();
+  }
+
+  public updateSelf(update: IWeeklySchedulerRange<number>) {
+    this.ngModelCtrl.$setViewValue(update);
+
+    this.schedulerCtrl.onChange({
+      itemIndex: this.itemIndex,
+      scheduleIndex: this.scheduleIndex,
+      scheduleValue: this.ngModelCtrl.$modelValue
+    });
   }
 }
 
 /** @internal */
-class WeeklySlotDirective implements angular.IDirective {
+class WeeklySlotComponent implements angular.IComponentOptions {
   static $name = 'weeklySlot';
-
-  controller = WeeklySlotController.$name;
-  controllerAs = WeeklySlotController.$controllerAs;
-
-  restrict = 'E';
-  require = ['^weeklyScheduler', 'ngModel', 'weeklySlot'];
-
-  scope = {
+  
+  bindings = {
+    config: '<',
     schedule: '=ngModel',
     itemIndex: '<',
     scheduleIndex: '<',
     item: '='
   };
 
+  controller = WeeklySlotController.$name;
+  controllerAs = WeeklySlotController.$controllerAs;
+
+  require = {
+    schedulerCtrl: '^weeklyScheduler',
+    ngModelCtrl: 'ngModel'
+  };
+
   templateUrl = 'ng-weekly-scheduler/weekly-slot/weekly-slot.html';
-
-  link = (scope, element: angular.IAugmentedJQuery, attrs: angular.IAttributes, ctrls) => {
-    var schedulerCtrl: WeeklySchedulerController = ctrls[0],
-        ngModelCtrl: angular.INgModelController = ctrls[1],
-        weeklySlotCtrl: WeeklySlotController = ctrls[2];
-
-    var conf = schedulerCtrl.config;
-    var containerEl = element.parent();
-    var resizeDirectionIsStart = true;
-    var valuesOnDragStart = {start: scope.schedule.start, end: scope.schedule.end};
-
-    var pixelToVal = function (pixel) {
-      var percent = pixel / containerEl[0].clientWidth;
-      return Math.floor(percent * (conf.intervalCount) + 0.5) * conf.interval;
-    };
-
-    var removeSchedule = function(schedule) {
-      var schedules = scope.item.schedules;
-
-      schedules.splice(schedules.indexOf(schedule), 1);
-    }
-
-    var mergeOverlaps = function () {
-      var schedule = scope.schedule;
-      var schedules = scope.item.schedules;
-      schedules.forEach(function (el) {
-        if (el !== schedule) {
-          // model is inside another slot
-          if (el.end >= schedule.end && el.start <= schedule.start) {
-            removeSchedule(el);
-
-            updateSelf({
-              start: el.start,
-              end: el.end
-            });
-          }
-          // model completely covers another slot
-          else if (schedule.end >= el.end && schedule.start <= el.start) {
-            removeSchedule(el);
-          }
-          // another slot's end is inside current model
-          else if (el.end >= schedule.start && el.end <= schedule.end) {
-            removeSchedule(el);
-
-            updateSelf({
-              start: el.start,
-              end: schedule.end
-            });
-          }
-          // another slot's start is inside current model
-          else if (el.start >= schedule.start && el.start <= schedule.end) {
-            removeSchedule(el);
-
-            updateSelf({
-              start: schedule.start,
-              end: el.end
-            });
-          }
-        }
-      });
-    };
-
-    var updateSelf = (update: IWeeklySchedulerRange<number>) => {
-      ngModelCtrl.$setViewValue(update);
-      schedulerCtrl.onChange({ itemIndex: scope.itemIndex, scheduleIndex: scope.scheduleIndex, scheduleValue: ngModelCtrl.$modelValue });
-    }
-
-    scope.canRemove = () => !angular.isDefined(scope.item.editable) || scope.item.editable;
-
-    if (scope.item.editable !== false) {
-      scope.deleteSelf = function () {
-        containerEl.removeClass('dragging');
-        containerEl.removeClass('slot-hover');
-        removeSchedule(scope.schedule);
-      };
-
-      scope.startResizeStart = function () {
-        resizeDirectionIsStart = true;
-        scope.startDrag();
-      };
-
-      scope.startResizeEnd = function () {
-        resizeDirectionIsStart = false;
-        scope.startDrag();
-      };
-
-      scope.startDrag = function () {
-        element.addClass('active');
-
-        containerEl.addClass('dragging');
-        containerEl.attr('no-add', 'true');
-
-        valuesOnDragStart = {
-          start: scope.schedule.start,
-          end: scope.schedule.end 
-        };
-      };
-
-      scope.endDrag = function () {
-
-        // this prevents user from accidentally
-        // adding new slot after resizing or dragging
-        setTimeout(function () {
-          containerEl.removeAttr('no-add');
-        }, 500);
-
-        element.removeClass('active');
-        containerEl.removeClass('dragging');
-
-        mergeOverlaps();
-      };
-
-      scope.resize = function (d) {
-        var ui = scope.schedule;
-        var delta = pixelToVal(d);
-
-        if (resizeDirectionIsStart) {
-          var newStart = Math.round(valuesOnDragStart.start + delta);
-
-          if (ui.start !== newStart && newStart <= ui.end - 1 && newStart >= 0) {
-            updateSelf({
-              start: newStart,
-              end: ui.end
-            });
-          }
-        } else {
-          var newEnd = Math.round(valuesOnDragStart.end + delta);
-
-          if (ui.end !== newEnd && newEnd >= ui.start + 1 && newEnd <= conf.maxValue) {
-            updateSelf({
-              start: ui.start,
-              end: newEnd
-            });
-          }
-        }
-      };
-
-      scope.drag = function (d) {
-        var ui = scope.schedule;
-        var delta = pixelToVal(d);
-        var duration = valuesOnDragStart.end - valuesOnDragStart.start;
-
-        var newStart = Math.round(valuesOnDragStart.start + delta);
-        var newEnd = Math.round(newStart + duration);
-
-        if (ui.start !== newStart && newStart >= 0 && newEnd <= conf.maxValue) {
-          updateSelf({
-            start: newStart,
-            end: newEnd
-          })
-        }
-      };
-    }
-
-    // on init, merge overlaps
-    mergeOverlaps();
-
-    //// UI -> model ////////////////////////////////////
-    ngModelCtrl.$parsers.push((ui) => {
-      ngModelCtrl.$modelValue.start = ui.start;
-      ngModelCtrl.$modelValue.end = ui.end;
-      
-      return ngModelCtrl.$modelValue;
-    });
-
-    scope.$on('weeklySchedulerLocaleChanged', function () {
-      // Simple change object reference so that ngModel triggers formatting & rendering
-      scope.schedule = angular.copy(scope.schedule);
-    });
-  }
-
-  static Factory() {
-    let directive = () => new WeeklySlotDirective();
-
-    return directive;
-  }
 }
 
 angular
   .module('weeklyScheduler')
   .controller(WeeklySlotController.$name, WeeklySlotController)
-  .directive(WeeklySlotDirective.$name, WeeklySlotDirective.Factory());
+  .component(WeeklySlotComponent.$name, new WeeklySlotComponent());
