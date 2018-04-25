f073987c9607900a3aedda320434d1d7461250dd
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-25 12:04:26 -0400

    It was browser zoom the whole time... *shame*

--- app/demo-app.ts
+++ app/demo-app.ts
@@ -27,33 +27,33 @@ angular.module('demoApp', ['ngAnimate', 'weeklyScheduler', 'weeklySchedulerI18N'
             label: 'Mon',
             //editable: false,
             schedules: [
-              // { start: 300, end: 1140 }
+              { start: 300, end: 1140 }
             ]
           },
           {
             label: 'Tue',
             schedules: [
-              // { start: 0, end: 240 },
-              // { start: 300, end: 360 }
+              { start: 0, end: 240 },
+              { start: 300, end: 360 }
             ]
           },
           {
             label: 'Wed',
             schedules: [
-              // { start: 120, end: 720 }
+              { start: 120, end: 720 }
             ]
           },
           {
             label: 'Thur',
             editable: false,
             schedules: [
-              // { start: 300, end: 1140 }
+              { start: 300, end: 1140 }
             ]
           },
           {
             label: 'Fri',
             schedules: [
-              // { start: 720, end: 780 }
+              { start: 720, end: 780 }
             ]
           },
           {
