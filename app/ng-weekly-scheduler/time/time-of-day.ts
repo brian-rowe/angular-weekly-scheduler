abb6b089558d99fcce6210c8e5446a7b8023c5c3
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-26 16:50:19 -0400

    You know what? We can just get rid of moment entirely, starting with the timeOfDay filter

--- app/ng-weekly-scheduler/time/time-of-day.ts
+++ app/ng-weekly-scheduler/time/time-of-day.ts
@@ -3,14 +3,16 @@ class TimeOfDayFilter {
     static $name = 'timeOfDay';
 
     public static Factory() {
-        const standardFormat: string = 'h:mmA';
-        const militaryFormat: string = 'HH:mm';
-
         return function(minutes: number): string {
-            // The moment-duration-format package always outputs military time, (it converts a duration to a time string, not a time of day) so we'll need to grab that and then convert
-            let militaryTime = moment.duration(minutes, 'minutes').format(militaryFormat, { trim: false });
+            let hours = Math.floor(minutes / 60);
+            let remainingMinutes = (minutes - (hours * 60)).toString();
+            let meridiem = hours > 11 ? 'P' : 'A';
 
-            return moment(militaryTime, militaryFormat).format(standardFormat);
+            if (remainingMinutes.length == 1) {
+                remainingMinutes += '0';
+            }
+
+            return `${hours % 12 || 12}:${remainingMinutes}${meridiem}`;
         }
     }
 }
