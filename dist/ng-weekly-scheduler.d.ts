ad88ddd05176c9afc9f1c4e1372612210251908e
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-27 09:02:01 -0400

    I "IWeeklySchedulerItem" inteface needs to be exported too

--- dist/ng-weekly-scheduler.d.ts
+++ dist/ng-weekly-scheduler.d.ts
@@ -1,3 +1,8 @@
+interface IWeeklySchedulerItem<T> {
+    label: string;
+    editable?: boolean;
+    schedules: IWeeklySchedulerRange<T>[];
+}
 interface IWeeklySchedulerRange<T> {
     start: number;
     end: number;
