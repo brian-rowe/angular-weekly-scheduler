0d9ffe98c377e55720120a8f77338494e6a3ebb6
Brian Rowe <brian.rowe@captiveaire.com>
2018-04-30 11:00:14 -0400

    Expose the IWeeklySchedulerOptions interface to clients

--- dist/ng-weekly-scheduler.d.ts
+++ dist/ng-weekly-scheduler.d.ts
@@ -3,6 +3,11 @@ interface IWeeklySchedulerItem<T> {
     editable?: boolean;
     schedules: IWeeklySchedulerRange<T>[];
 }
+interface IWeeklySchedulerOptions {
+    editSlot?: (schedule: IWeeklySchedulerRange<any>) => void;
+    monoSchedule?: boolean;
+    interval?: number;
+}
 interface IWeeklySchedulerRange<T> {
     start: number;
     end: number;
