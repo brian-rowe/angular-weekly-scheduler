73b1f07ccc1ef882d6bddc591ce0d80116d1805e
Brian Rowe <brian.rowe@captiveaire.com>
2018-05-03 15:44:28 -0400

    Add failing tests for considering fullCalendar

--- test/spec/schedule_validator_service_test.ts
+++ test/spec/schedule_validator_service_test.ts
@@ -25,6 +25,50 @@ describe('schedule validator service', function () {
     }
 
     describe('should validate', function() {
+        describe('calendars with gaps', () => {
+            it('as valid when fullCalendar is false', function() {
+                let item = getTestItem([
+                    { start: 0, end: 60, value: true },
+                    { start: 75, end: 120, value: true } 
+                ]);
+
+                expect($service.areSchedulesValid(item, testConfig)).toBeTruthy();
+            });
+
+            it('as invalid when fullCalendar is true', function () {
+                let item = getTestItem([
+                    { start: 0, end: 60, value: true },
+                    { start: 75, end: 120, value: true } 
+                ]);
+
+                let config = angular.extend(testConfig, { fullCalendar: true });
+
+                expect($service.areSchedulesValid(item, config)).toBeFalsy();
+            });
+        });
+
+        describe('calendars without gaps', () => {
+            it('as valid when fullCalendar is false', function() {
+                let item = getTestItem([
+                    { start: 0, end: 720, value: true },
+                    { start: 720, end: 1440, value: true } 
+                ]);
+
+                expect($service.areSchedulesValid(item, testConfig)).toBeTruthy();
+            });
+
+            it('as valid when fullCalendar is true', function () {
+                let item = getTestItem([
+                    { start: 0, end: 720, value: true },
+                    { start: 720, end: 1440, value: true } 
+                ]);
+
+                let config = angular.extend(testConfig, { fullCalendar: true });
+
+                expect($service.areSchedulesValid(item, config)).toBeTruthy();
+            });
+        });
+
         describe('non-touching schedules', function () {
             it('with the same value as valid', function() {
                 let item = getTestItem([
