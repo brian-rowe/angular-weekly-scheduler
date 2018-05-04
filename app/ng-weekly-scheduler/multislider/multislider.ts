dfbae51758303b94ab78a5f8491b25b881406b34
Brian Rowe <brian.rowe@captiveaire.com>
2018-05-04 09:09:50 -0400

    Don't mergeOverlaps $onInit -- we'll come up with something else, with all the new handler code this is breaking more complex examples, there needs to be something simpler to merge consecutive ranges with the same value

--- app/ng-weekly-scheduler/multislider/multislider.ts
+++ app/ng-weekly-scheduler/multislider/multislider.ts
@@ -44,8 +44,6 @@ class MultiSliderController implements angular.IComponentController {
   public size: number = 60; // minutes
   
   $onInit() {
-    this.item.schedules.forEach(s => this.mergeOverlaps(s));
-
     this.$scope.$on(WeeklySchedulerEvents.RESIZED, () => {
       this.resize();
     });
