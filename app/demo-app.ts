4b6ee5835d6eab337667ae48931938677e2435f7
Brian Rowe <brian.rowe@captiveaire.com>
2018-05-04 11:09:29 -0400

    Build

--- app/demo-app.ts
+++ app/demo-app.ts
@@ -54,7 +54,7 @@ angular.module('demoApp', ['weeklyScheduler'])
           // },
           {
             label: 'Sat',
-            schedules: JSON.parse('[{"$class":"rangevalue1","start":0,"end":255,"value":1},{"$class":"rangevalue0","start":255,"end":360,"value":0},{"$class":"rangevalue1","start":360,"end":480,"value":1},{"$class":"rangevalue0","start":480,"end":1425,"value":0},{"$class":"rangevalue2","start":1425,"end":0,"value":2}]');
+            schedules: JSON.parse('[{"$class":"rangevalue1","start":0,"end":255,"value":1},{"$class":"rangevalue0","start":255,"end":360,"value":0},{"$class":"rangevalue1","start":360,"end":480,"value":1},{"$class":"rangevalue0","start":480,"end":1425,"value":0},{"$class":"rangevalue2","start":1425,"end":0,"value":2}]')
           }
         ]
       };
