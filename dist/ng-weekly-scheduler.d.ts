03c0da57ccff055051cbfed8512d6daf9a3cfb4a
Brian Rowe <brian.rowe@captiveaire.com>
2018-05-07 13:28:51 -0400

    Build

--- dist/ng-weekly-scheduler.d.ts
+++ dist/ng-weekly-scheduler.d.ts
@@ -19,6 +19,8 @@ interface IWeeklySchedulerRange<T> {
     $class?: string;
     /** This will indicate whether the item is currently considered active to the UI */
     $isActive?: boolean;
+    /** This will indicate whether the item is currently being edited by the user */
+    $isEditing?: boolean;
     start: number;
     end: number;
     value: T;
