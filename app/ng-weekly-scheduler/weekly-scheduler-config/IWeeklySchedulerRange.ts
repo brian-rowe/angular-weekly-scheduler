b117c9efaaee5fee3fab7aa62548f0e727633d85
Brian Rowe <brian.rowe@captiveaire.com>
2018-05-07 13:28:34 -0400

    - Add 'pending' style for weekly-slot - Add $isEditing for IWeeklySchedulerRange

--- app/ng-weekly-scheduler/weekly-scheduler-config/IWeeklySchedulerRange.ts
+++ app/ng-weekly-scheduler/weekly-scheduler-config/IWeeklySchedulerRange.ts
@@ -5,6 +5,9 @@ interface IWeeklySchedulerRange<T> {
     /** This will indicate whether the item is currently considered active to the UI */
     $isActive?: boolean;
 
+    /** This will indicate whether the item is currently being edited by the user */
+    $isEditing?: boolean;
+
     start: number;
     end: number;
 
