abd1249f62dcbedbd58161b517406c8da85e5634
Brian Rowe <brian.rowe@captiveaire.com>
2018-05-08 13:18:12 -0400

    - Add new failing test cases that verify that FullCalendar validator makes sure that not only are there no gaps, but that the schedules begin and end at midnight - Adjust current test cases to not fail for this reason - Fix code to make all tests pass

--- app/ng-weekly-scheduler/schedule-validator/full-calendar-validator-service.ts
+++ app/ng-weekly-scheduler/schedule-validator/full-calendar-validator-service.ts
@@ -10,12 +10,22 @@ class FullCalendarValidatorService {
         // When this option is true we should enforce that there are no gaps in the schedules
 
         // Compare two at a time until the end;
-        let len = schedules.length;
+        let len = schedules.length - 1;
         let result = true;
 
-        for (let i = 0; i < len - 1; i++) {
+        for (let i = 0; i < len; i++) {
             let current = schedules[i];
             let next = schedules[i + 1];
+            
+            // Validate that the first item lands at 0
+            if (i === 0 && current.start !== 0) {
+                return false;
+            }
+
+            // Validate that the last item lands at maxValue
+            if (i === len - 1 && next.end !== 1440) { // TODO
+                return false;
+            }
 
             result = result && current.end === next.start;
         }
--- test/spec/full_calendar_validator_service_test.ts
+++ test/spec/full_calendar_validator_service_test.ts
@@ -10,11 +10,45 @@ describe('full calendar validator service', function () {
     }));
 
     describe('should validate', function () {
+        describe('calendars whose items do not begin at the start', () => {
+            let offStartNoGaps = [
+                { start: 30, end: 60, value: true },
+                { start: 60, end: 720, value: true },
+                { start: 720, end: 1440, value: true }
+            ]; 
+
+            it('as valid when fullCalendar is false', () => {
+                expect($service.validate(offStartNoGaps, false)).toBeTruthy();
+            });
+
+            it('as invalid when fullCalendar is true', () => {
+                debugger
+                expect($service.validate(offStartNoGaps, true)).toBeFalsy();
+            });
+        });
+
+        describe('calendars whose items do not end at the end', () => {
+            let offEndNoGaps = [
+                { start: 0, end: 60, value: true },
+                { start: 60, end: 720, value: true },
+                { start: 720, end: 1395, value: true }
+            ]; 
+
+            it('as valid when fullCalendar is false', () => {
+                expect($service.validate(offEndNoGaps, false)).toBeTruthy();
+            });
+
+            it('as invalid when fullCalendar is true', () => {
+                expect($service.validate(offEndNoGaps, true)).toBeFalsy();
+            });
+        });
+
+
         describe('calendars with gaps', () => {
             let withGaps = [
                 { start: 0, end: 60, value: true },
                 { start: 75, end: 120, value: true },
-                { start: 120, end: 1395, value: true }
+                { start: 120, end: 1440, value: true }
             ];
 
             it('as valid when fullCalendar is false', function () {
