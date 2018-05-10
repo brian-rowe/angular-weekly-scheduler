72752acfe7e2efcc9a41c4d26ec534267c3e8c82
Brian Rowe <browe404@gmail.com>
2018-05-10 19:08:13 -0400

    Add a failing test for the days it SHOULD be rendering regardless of whether or not we actually have items for them

--- test/spec/weekly_scheduler_test.ts
+++ test/spec/weekly_scheduler_test.ts
@@ -23,16 +23,28 @@ describe('weekly scheduler', () => {
             expect(element).toBeTruthy();
         });
 
-        describe('24 hours worth of time slots: ', () => {
-            let timeslots = [
+        describe('24 hour: ', () => {
+            let hours = [
                 '12a', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a', '11a', '12a',
                 '12p', '1p', '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', '10p', '11p', '12p',
             ];
 
-            angular.forEach(timeslots, (timeslot) => {
-                describe(timeslot, () => {
+            angular.forEach(hours, (hour) => {
+                describe(hour, () => {
                     it('renders', () => {
-                        expect($element.text()).toContain(timeslot);
+                        expect($element.text()).toContain(hour);
+                    });
+                });
+            });
+        });
+
+        describe('seven days: ', () => {
+            let days = ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
+
+            angular.forEach(days, (day) => {
+                describe(day, () => {
+                    it('renders', () => {
+                        expect($element.text()).toContain(day);
                     });
                 });
             });
