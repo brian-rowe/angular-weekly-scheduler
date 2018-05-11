a0076776c493050788ae67ad03038bf0f9f951c3
Brian Rowe <brian.rowe@captiveaire.com>
2018-05-11 13:09:03 -0400

    Set up demo-app to help resolve a bug I introduced today...

--- app/demo-app.ts
+++ app/demo-app.ts
@@ -55,7 +55,9 @@ angular.module('demoApp', ['weeklyScheduler'])
           // },
           {
             day: Days.Saturday,
-            schedules: JSON.parse('[{"$class":"rangevalue1","start":0,"end":255,"value":1},{"$class":"rangevalue0","start":255,"end":360,"value":0},{"$class":"rangevalue1","start":360,"end":480,"value":1},{"$class":"rangevalue0","start":480,"end":1425,"value":0},{"$class":"rangevalue2","start":1425,"end":0,"value":2}]')
+            schedules: [
+              { start: 300, end: 900 }
+            ]
           }
         ] as IWeeklySchedulerItem<any>[]
       };
