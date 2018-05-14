ec8ca4ccb35ccc29a02151fcd98e7963fc4d6fd0
Brian Rowe <brian.rowe@captiveaire.com>
2018-05-14 09:17:41 -0400

    Build

--- dist/ng-weekly-scheduler.d.ts
+++ dist/ng-weekly-scheduler.d.ts
@@ -1,15 +1,4 @@
 /// <reference types="angular" />
-/**
- * We should be able to convert the schedules beforehand, pass just the schedules in and have this package build the items
- * This helps reduce code duplication in clients.
- * This is used as a substitute for lodash.groupBy to keep the footprint small
- */
-declare class GroupService {
-    static $name: string;
-    groupSchedules(schedules: IWeeklySchedulerRange<any>[]): {
-        [key: number]: IWeeklySchedulerRange<any>[];
-    };
-}
 declare const enum Days {
     Monday = 0,
     Tuesday = 1,
