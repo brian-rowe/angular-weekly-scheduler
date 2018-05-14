be6773f6fd3e8737660cffdab407c3293596e9d7
Brian Rowe <brian.rowe@captiveaire.com>
2018-05-14 10:12:51 -0400

    Try this for mobile touch

--- app/ng-weekly-scheduler/handle/handle.ts
+++ app/ng-weekly-scheduler/handle/handle.ts
@@ -16,7 +16,7 @@ class HandleDirective implements angular.IDirective {
     let mousedownEvent: string = 'mousedown touchstart';
     let mousemoveEvent: string = 'mousemove touchmove';
     let mouseupEvent: string = 'mouseup touchend';
-    
+
     element.on(mousedownEvent, (event) => {
       // Prevent default dragging of selected content
       event.preventDefault();
@@ -32,7 +32,8 @@ class HandleDirective implements angular.IDirective {
     });
 
     function mousemove(event) {
-      var delta = event.pageX - x;
+      let pageX = this.getTouches(event)[0].clientX;
+      var delta = pageX - x;
 
       if (angular.isFunction(scope.ondrag)) {
         scope.$apply(scope.ondrag({ delta: delta }));
@@ -47,6 +48,22 @@ class HandleDirective implements angular.IDirective {
         scope.$apply(scope.ondragstop());
       }
     }
+  }
+
+  private getTouches(event: any): any { // todo
+    if (event.originalEvent) {
+      if (event.originalEvent.touches && event.originalEvent.touches.length) {
+        return event.originalEvent.touches;
+      } else if (event.originalEvent.changedTouches && event.originalEvent.changedTouches.length) {
+        return event.originalEvent.changedTouches;
+      }
+    }
+
+    if (!event.touches) {
+      event.touches = [event.originalEvent];
+    }
+
+    return event.touches;
   }
 
   constructor(
