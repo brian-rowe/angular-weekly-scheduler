854787f7589542498d806d47c6a5491ead2346c6
Brian Rowe <brian.rowe@captiveaire.com>
2018-06-04 09:17:11 -0400

    Set up demo-app to demonstrate problem with mergeOverlaps

--- app/demo-app.ts
+++ app/demo-app.ts
@@ -69,20 +69,7 @@ angular.module('demoApp', ['br.weeklyScheduler'])
         }
       ]);
 
-      $scope.adapterTwo = new DemoAdapter([
-        {
-          day: br.weeklyScheduler.Days.Friday,
-          start: 0,
-          end: 360,
-          value: false
-        },
-        {
-          day: br.weeklyScheduler.Days.Friday,
-          start: 360,
-          end: 1440,
-          value: false
-        }
-      ]);
+      $scope.adapterTwo = new DemoAdapter(JSON.parse('[{"$class":"Unoccupied","day":3,"start":0,"end":135,"value":"Unoccupied"},{"$class":"Setpointd0f1df","day":3,"start":135,"end":195,"value":"Setpointd0f1df"},{"$class":"Unoccupied","day":3,"start":195,"end":225,"value":"Unoccupied"},{"$class":"Unoccupied","day":3,"start":225,"end":270,"value":"Unoccupied"},{"$class":"Setpointd0f1df","day":3,"start":270,"end":330,"value":"Setpointd0f1df"},{"$class":"Setpointd0f1df","day":3,"start":330,"end":360,"value":"Setpointd0f1df"},{"$class":"Setpointd0f1df","day":3,"start":360,"end":510,"value":"Setpointd0f1df"},{"$class":"Unoccupied","day":3,"start":510,"end":585,"value":"Unoccupied"},{"$class":"Unoccupied","day":3,"start":585,"end":630,"value":"Unoccupied"},{"$class":"Unoccupied","day":3,"start":630,"end":675,"value":"Unoccupied"},{"$class":"Unoccupied","day":3,"start":675,"end":690,"value":"Unoccupied"},{"$class":"Unoccupied","day":3,"start":690,"end":810,"value":"Unoccupied"},{"$class":"Unoccupied","day":3,"start":810,"end":0,"value":"Unoccupied"}]'));
 
       $scope.saveAll = function () {
         $scope.result = JSON.stringify($scope.adapter.getSnapshot()) + JSON.stringify($scope.adapterTwo.getSnapshot());
