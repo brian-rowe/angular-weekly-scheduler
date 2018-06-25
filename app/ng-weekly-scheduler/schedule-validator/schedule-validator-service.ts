6e58d106eef2e8b63ec1ccfea706c000a76b2caf
Brian Rowe <brian.rowe@captiveaire.com>
2018-06-25 13:51:25 -0400

    Clean up some existing scheduleValidationService code

--- app/ng-weekly-scheduler/schedule-validator/schedule-validator-service.ts
+++ app/ng-weekly-scheduler/schedule-validator/schedule-validator-service.ts
@@ -3,17 +3,13 @@ class ScheduleValidationService {
     static $name = 'brWeeklySchedulerValidationService';
 
     static $inject = [
-        'brWeeklySchedulerFullCalendarValidatorService',
         'brWeeklySchedulerMaxTimeSlotValidatorService',
-        'brWeeklySchedulerMonoScheduleValidatorService',
         'brWeeklySchedulerNullEndValidatorService',
         'brWeeklySchedulerOverlapValidatorService'
     ]
 
     private constructor(
-        private fullCalendarValidatorService: ValidatorService,
         private maxTimeSlotValidatorService: ValidatorService,
-        private monoScheduleValidatorService: ValidatorService,
         private nullEndScheduleValidatorService: ValidatorService,
         private overlapValidatorService: ValidatorService
     ) {
@@ -22,9 +18,7 @@ class ScheduleValidationService {
     public getValidationErrors(item: br.weeklyScheduler.IWeeklySchedulerItem<any>, config: IWeeklySchedulerConfig<any>): ValidationError[] {
         let validators: ValidatorService[] = [
             this.maxTimeSlotValidatorService,
-            this.monoScheduleValidatorService,
             this.nullEndScheduleValidatorService,
-            //this.fullCalendarValidatorService,
             this.overlapValidatorService
         ];
 
