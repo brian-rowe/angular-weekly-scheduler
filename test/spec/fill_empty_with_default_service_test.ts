f97ea53832db56957e0d34f1dacfd099d8b0f6cc
Brian Rowe <brian.rowe@captiveaire.com>
2018-09-17 17:08:57 -0400

    fix tests to go with new objects

--- test/spec/fill_empty_with_default_service_test.ts
+++ test/spec/fill_empty_with_default_service_test.ts
@@ -1,11 +1,14 @@
+/// <reference path="../../app/ng-weekly-scheduler/weekly-scheduler-item/WeeklySchedulerItemFactory.ts" />
 /// <reference path="../../app/ng-weekly-scheduler/fill-empty-with-default/fill-empty-with-default-service.ts" />
 
 describe('fillEmptyWithDefault service', () => {
+    var $itemFactory: WeeklySchedulerItemFactory;
     var $service: FillEmptyWithDefaultService;
 
     beforeEach(angular.mock.module('demoApp'));
 
-    beforeEach(inject(function(_brWeeklySchedulerFillEmptyWithDefaultService_) {
+    beforeEach(inject(function(_brWeeklySchedulerItemFactory_, _brWeeklySchedulerFillEmptyWithDefaultService_) {
+        $itemFactory = _brWeeklySchedulerItemFactory_;
         $service = _brWeeklySchedulerFillEmptyWithDefaultService_;
     }));
 
@@ -24,143 +27,143 @@ describe('fillEmptyWithDefault service', () => {
 
         describe('should work when', () => {
             it('there are no starting schedules', () => {
-                let item = config.createItem(0, []);
+                let item = $itemFactory.createItem(config, 0, []);
 
-                let expectedResult = [
+                let expectedResult = $itemFactory.createItem(config, 0,  [
                     { day: 0, start: 0, end: 0, value: config.defaultValue }
-                ];
+                ]);
     
                 let actualResult = $service.fill(item, config);
     
-                expect(angular.equals(actualResult, expectedResult)).toBeTruthy();  
+                expect(angular.equals(actualResult, expectedResult.schedules)).toBeTruthy();  
             });
 
             describe('there is only one starting schedule', () => {
                 it('that starts at 0', () => {
-                    let item = config.createItem(0, [
+                    let item = $itemFactory.createItem(config, 0, [
                         { day: 0, start: 0, end: 720, value: true }
                     ]);
         
-                    let expectedResult = [
+                    let expectedResult = $itemFactory.createItem(config, 0,  [
                         { day: 0, start: 0, end: 720, value: true },
                         { day: 0, start: 720, end: 0, value: config.defaultValue },
-                    ];
+                    ]);
         
                     let actualResult = $service.fill(item, config);
         
-                    expect(angular.equals(actualResult, expectedResult)).toBeTruthy(); 
+                    expect(angular.equals(actualResult, expectedResult.schedules)).toBeTruthy(); 
                 });
 
                 it('that starts & ends in the middle', () => {
-                    let item = config.createItem(0, [
+                    let item = $itemFactory.createItem(config, 0, [
                         { day: 0, start: 180, end: 720, value: true }
                     ]);
         
-                    let expectedResult = [
+                    let expectedResult = $itemFactory.createItem(config, 0,  [
                         { day: 0, start: 0, end: 180, value: config.defaultValue },
                         { day: 0, start: 180, end: 720, value: true },
                         { day: 0, start: 720, end: 0, value: config.defaultValue },
-                    ];
+                    ]);
         
                     let actualResult = $service.fill(item, config);
         
-                    expect(angular.equals(actualResult, expectedResult)).toBeTruthy(); 
+                    expect(angular.equals(actualResult, expectedResult.schedules)).toBeTruthy(); 
                 });
 
                 it ('that ends at 0', () => {
-                    let item = config.createItem(0, [
+                    let item = $itemFactory.createItem(config, 0, [
                         { day: 0, start: 720, end: 0, value: true }
                     ]);
         
-                    let expectedResult = [
+                    let expectedResult = $itemFactory.createItem(config, 0,  [
                         { day: 0, start: 0, end: 720, value: config.defaultValue },
                         { day: 0, start: 720, end: 0, value: true },
-                    ];
+                    ]);
         
                     let actualResult = $service.fill(item, config);
         
-                    expect(angular.equals(actualResult, expectedResult)).toBeTruthy(); 
+                    expect(angular.equals(actualResult, expectedResult.schedules)).toBeTruthy(); 
                 });
             });
 
             it('there are two starting schedules', () => {
-                let item = config.createItem(0, [
+                let item = $itemFactory.createItem(config, 0, [
                     { day: 0, start: 0, end: 720, value: true },
                     { day: 0, start: 780, end: 0, value: true }
                 ]);
     
-                let expectedResult = [
+                let expectedResult = $itemFactory.createItem(config, 0,  [
                     { day: 0, start: 0, end: 720, value: true },
                     { day: 0, start: 720, end: 780, value: config.defaultValue },
                     { day: 0, start: 780, end: 0, value: true },
-                ];
+                ]);
     
                 let actualResult = $service.fill(item, config);
     
-                expect(angular.equals(actualResult, expectedResult)).toBeTruthy();
+                expect(angular.equals(actualResult, expectedResult.schedules)).toBeTruthy();
             });
 
             describe('there are three or more starting schedules', () => {
                 it('that start and end on the extremes', () => {
-                    let item = config.createItem(0, [
+                    let item = $itemFactory.createItem(config, 0, [
                         { day: 0, start: 0, end: 75, value: true },
                         { day: 0, start: 330, end: 840, value: true },
                         { day: 0, start: 1410, end: 0, value: true }
                     ]);
         
-                    let expectedResult = [
+                    let expectedResult = $itemFactory.createItem(config, 0,  [
                         { day: 0, start: 0, end: 75, value: true },
                         { day: 0, start: 75, end: 330, value: false },
                         { day: 0, start: 330, end: 840, value: true },
                         { day: 0, start: 840, end: 1410, value: false },
                         { day: 0, start: 1410, end: 0, value: true }
-                    ];
+                    ]);
         
                     let actualResult = $service.fill(item, config);
         
-                    expect(angular.equals(actualResult, expectedResult)).toBeTruthy();
+                    expect(angular.equals(actualResult, expectedResult.schedules)).toBeTruthy();
                 });
 
                 it('that do not start at 0', () => {
-                    let item = config.createItem(0, [
+                    let item = $itemFactory.createItem(config, 0, [
                         { day: 0, start: 30, end: 75, value: true },
                         { day: 0, start: 330, end: 840, value: true },
                         { day: 0, start: 1410, end: 0, value: true }
                     ]);
         
-                    let expectedResult = [
+                    let expectedResult = $itemFactory.createItem(config, 0,  [
                         { day: 0, start: 0, end: 30, value: false },
                         { day: 0, start: 30, end: 75, value: true },
                         { day: 0, start: 75, end: 330, value: false },
                         { day: 0, start: 330, end: 840, value: true },
                         { day: 0, start: 840, end: 1410, value: false },
                         { day: 0, start: 1410, end: 0, value: true }
-                    ];
+                    ]);
         
                     let actualResult = $service.fill(item, config);
         
-                    expect(angular.equals(actualResult, expectedResult)).toBeTruthy();
+                    expect(angular.equals(actualResult, expectedResult.schedules)).toBeTruthy();
                 });
 
                 it('that do not end at 0', () => {
-                    let item = config.createItem(0, [
+                    let item = $itemFactory.createItem(config, 0, [
                         { day: 0, start: 0, end: 75, value: true },
                         { day: 0, start: 330, end: 840, value: true },
                         { day: 0, start: 1410, end: 1425, value: true }
                     ]);
         
-                    let expectedResult = [
+                    let expectedResult = $itemFactory.createItem(config, 0,  [
                         { day: 0, start: 0, end: 75, value: true },
                         { day: 0, start: 75, end: 330, value: false },
                         { day: 0, start: 330, end: 840, value: true },
                         { day: 0, start: 840, end: 1410, value: false },
                         { day: 0, start: 1410, end: 1425, value: true },
                         { day: 0, start: 1425, end: 0, value: false }
-                    ];
+                    ]);
         
                     let actualResult = $service.fill(item, config);
         
-                    expect(angular.equals(actualResult, expectedResult)).toBeTruthy();
+                    expect(angular.equals(actualResult, expectedResult.schedules)).toBeTruthy();
                 });
             });
         });
--- test/spec/purge_default_service_test.ts
+++ test/spec/purge_default_service_test.ts
@@ -1,11 +1,14 @@
+/// <reference path="../../app/ng-weekly-scheduler/weekly-scheduler-item/WeeklySchedulerItemFactory.ts" />
 /// <reference path="../../app/ng-weekly-scheduler/purge-default/purge-default-service.ts" />
 
 describe('purgeDefault service', () => {
+    var $itemFactory: WeeklySchedulerItemFactory;
     var $service: PurgeDefaultService;
 
     beforeEach(angular.mock.module('demoApp'));
 
-    beforeEach(inject(function(_brWeeklySchedulerPurgeDefaultService_) {
+    beforeEach(inject(function(_brWeeklySchedulerItemFactory_, _brWeeklySchedulerPurgeDefaultService_) {
+        $itemFactory = _brWeeklySchedulerItemFactory_;
         $service = _brWeeklySchedulerPurgeDefaultService_;
     }));
 
@@ -24,21 +27,21 @@ describe('purgeDefault service', () => {
             }
 
             it ('should return a full day of schedules with default-valued slots missing', () => {
-                let schedules = [
+                let item = $itemFactory.createItem(config, 0, [
                     { day: 0, start: 0, end: 720, value: true },
                     { day: 0, start: 720, end: 780, value: config.defaultValue },
                     { day: 0, start: 780, end: 900, value: true },
                     { day: 0, start: 900, end: config.maxValue, value: config.defaultValue }
-                ];
+                ]);
 
-                let expectedResult = [
+                let expectedResult = $itemFactory.createItem(config, 0, [
                     { day: 0, start: 0, end: 720, value: true },
                     { day: 0, start: 780, end: 900, value: true }
-                ];
+                ]);
 
-                let actualResult = $service.purge(schedules, config);
+                let actualResult = $service.purge(item.schedules, config);
 
-                expect(angular.equals(actualResult, expectedResult)).toBeTruthy();
+                expect(angular.equals(actualResult, expectedResult.schedules)).toBeTruthy();
             });
         });
     });
