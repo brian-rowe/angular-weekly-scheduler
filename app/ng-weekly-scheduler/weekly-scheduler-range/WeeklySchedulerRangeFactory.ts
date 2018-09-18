14113e73a10f0557a114917b64a0ffc4259f1b64
Brian Rowe <brian.rowe@captiveaire.com>
2018-09-18 14:04:34 -0400

    Create a (currently unutilized) WeeklySchedulerRangeFactory -- purpose incoming

---
+++ app/ng-weekly-scheduler/weekly-scheduler-range/WeeklySchedulerRangeFactory.ts
@@ -0,0 +1,12 @@
+/** @internal */
+class WeeklySchedulerRangeFactory {
+    static $name = 'brWeeklySchedulerRangeFactory';
+
+    public createItem(config: IWeeklySchedulerConfig<any>, schedule: br.weeklyScheduler.IWeeklySchedulerRange<any>) {
+        return new WeeklySchedulerRange(schedule);
+    }
+}
+
+angular
+    .module('br.weeklyScheduler')
+    .service(WeeklySchedulerRangeFactory.$name, WeeklySchedulerRangeFactory);
