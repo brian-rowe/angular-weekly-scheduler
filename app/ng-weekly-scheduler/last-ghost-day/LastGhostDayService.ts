e301f676474407bc7c705de2abf4ca278d168d24
Brian Rowe <brian.rowe@captiveaire.com>
2018-09-28 15:52:05 -0400

    begin refactoring LastGhostDayService while keeping tests passing

--- app/ng-weekly-scheduler/last-ghost-day/LastGhostDayService.ts
+++ app/ng-weekly-scheduler/last-ghost-day/LastGhostDayService.ts
@@ -7,30 +7,10 @@ class LastGhostDayService {
      * We should remove the ghost from that extreme. This will help grab the correct day
      */
     public getLastGhostDay(items: WeeklySchedulerItem<any>[]) {
-        // get the index of the $isGhostOrigin item
-        let originIndex;
-        let len = items.length;
-
-        for (let i = 0; i < len; i++) {
-            let currentItem = items[i];
-
-            if (currentItem.$isGhostOrigin) {
-                originIndex = i;
-                break;
-            }
-        }
+        let originIndex = this.getOriginIndex(items);
+        let renderedGhostIndices = this.getRenderedGhostIndices(items);
 
         // determine if the other $renderGhost items are above or below the $isGhostOrigin item
-        let renderedGhostIndices = [];
-
-        for (let i = 0; i < len; i++) {
-            let currentItem = items[i];
-
-            if (currentItem.$renderGhost) {
-                renderedGhostIndices.push(i);
-            }
-        }
-
         let above = renderedGhostIndices.every(i => i <= originIndex);
 
         // take first item for above or last item for below
@@ -43,6 +23,39 @@ class LastGhostDayService {
         }
 
         return result;
+    }
+
+    /** Get the index of the $isGhostOrigin item */
+    private getOriginIndex(items: WeeklySchedulerItem<any>[]) {
+        let originIndex;
+        let len = items.length;
+
+        for (let i = 0; i < len; i++) {
+            let currentItem = items[i];
+
+            if (currentItem.$isGhostOrigin) {
+                originIndex = i;
+                break;
+            }
+        }
+
+        return originIndex;
+    }
+
+    /** Get all of the item indices that currently have ghosts rendered */
+    private getRenderedGhostIndices(items: WeeklySchedulerItem<any>[]) {
+        let renderedGhostIndices = [];
+        let len = items.length;
+
+        for (let i = 0; i < len; i++) {
+            let currentItem = items[i];
+
+            if (currentItem.$renderGhost) {
+                renderedGhostIndices.push(i);
+            }
+        }
+
+        return renderedGhostIndices;
     }
 }
 
