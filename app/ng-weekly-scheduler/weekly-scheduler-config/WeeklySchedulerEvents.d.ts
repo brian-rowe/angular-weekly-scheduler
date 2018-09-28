4710aea5b412e6effe9dc103b8d01dc0e7338be9
Brian Rowe <brian.rowe@captiveaire.com>
2018-09-28 14:51:57 -0400

    all the events should have prefixes in case another library is being used with the same strings

--- app/ng-weekly-scheduler/weekly-scheduler-config/WeeklySchedulerEvents.d.ts
+++ app/ng-weekly-scheduler/weekly-scheduler-config/WeeklySchedulerEvents.d.ts
@@ -1,16 +1,16 @@
 declare const enum WeeklySchedulerEvents {
-    CLICK_ON_A_CELL = 'clickOnACell',
-    RESIZED = 'resized',
-    ZOOMED_IN = 'zoomedIn', // ack
-    ZOOMED_OUT = 'zoomedOut', //ack
-    RESET_ZOOM = 'resetZoom', // command
-    ZOOM_IN = 'zoomIn', // command
+    CLICK_ON_A_CELL = 'brWeeklyScheduler.clickOnACell',
+    RESIZED = 'brWeeklyScheduler.resized',
+    ZOOMED_IN = 'brWeeklyScheduler.zoomedIn', // ack
+    ZOOMED_OUT = 'brWeeklyScheduler.zoomedOut', //ack
+    RESET_ZOOM = 'brWeeklyScheduler.resetZoom', // command
+    ZOOM_IN = 'brWeeklyScheduler.zoomIn', // command
 
-    SLOT_DRAGGED = 'slotDragged',
-    DRAG_ENDED = 'dragEnded',
+    SLOT_DRAGGED = 'brWeeklyScheduler.slotDragged',
+    DRAG_ENDED = 'brWeeklyScheduler.dragEnded',
 
-    GHOST_DRAG_ENDED = 'ghostDragEnded',
-    COMMIT_GHOST = 'commitGhost',
-    REMOVE_LAST_GHOST = 'removeLastGhost',
-    REMOVE_GHOST = 'removeGhost'
+    GHOST_DRAG_ENDED = 'brWeeklyScheduler.ghostDragEnded',
+    COMMIT_GHOST = 'brWeeklyScheduler.commitGhost',
+    REMOVE_LAST_GHOST = 'brWeeklyScheduler.removeLastGhost',
+    REMOVE_GHOST = 'brWeeklyScheduler.removeGhost'
 }
