076d9f90d76e97d932b8cb6c9196d6aa8c2639dd
Rosalyn Rowe <rosalyn.rowe@captiveaire.com>
2019-07-29 15:46:56 -0400

    minor change -- demo app doesn't need to be wrapped in a function

--- src/demo-app.ts
+++ src/demo-app.ts
@@ -2,13 +2,9 @@ import * as angular from 'angular'
 import AppModule from './ng-weekly-scheduler/app/module';
 import { DemoController } from './ng-weekly-scheduler/demo/DemoController';
 
-function application() {
-  angular.module('demoApp', [AppModule])
-    .controller(DemoController.$name, DemoController);
+angular.module('demoApp', [AppModule])
+  .controller(DemoController.$name, DemoController);
 
-  angular.element(document).ready(() => {
-    angular.bootstrap(document, ['demoApp']);
-  });
-}
-
-application();
+angular.element(document).ready(() => {
+  angular.bootstrap(document, ['demoApp']);
+});
